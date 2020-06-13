1f45a7e0d16eb152366f0a3c02eb6328a9f8bda4
Arturo Sabates <sabates1988@outlook.com>
2020-06-13 15:30:01 -0400

    Fixed backend/frontend now subjects post and display on website

--- models/subject.model.js
+++ models/subject.model.js
@@ -3,12 +3,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const subjectSchema = new Schema({
+    username: { type: String, required: true },
     subject: { type: String, required: true },
     description: { type: String, required: true },
-    duration: { type: Number, required: true },
-    
-}, {
-    timestamps: true,
+    duration: { type: Number, required: true }
 });
 
 const Subject = mongoose.model('Subject', subjectSchema);
