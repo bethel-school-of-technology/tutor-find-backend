453d8191469037ee1de0b82dc97ad9314566cca1
Arturo Sabates <sabates1988@outlook.com>
2020-06-19 02:12:27 -0400

    authentication

--- routes/users.js
+++ routes/users.js
@@ -22,13 +22,21 @@ router.route('/add').post((req, res) => {
 });
 
 router.route('/login').post((req, res) => {
-    const username = req.body.userName;
-    const password = req.body.password;
 
-        User.findbyName({username: username})
+        const username = req.body.userName;
+        const password = req.body.password;
+
+          User.find({
+              username: username,
+              password: password
+        })
+          .then(users => res.json(users))
+          .catch(err => res.status(400).json('Error: ' + err));
+        });
+
+    /*newUser.save()
         .then(() => res.json('User added!'))
-        .catch(err => res.status(400).json('Error: ' + err));
-    });
+        .catch(err => res.status(400).json('Error: ' + err));*/
 
 
 module.exports = router;
